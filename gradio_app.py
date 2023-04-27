44fa31547d92ebdc13bfe17f50556581a358403f
Matthew Tucker <matt@joinpaladin.com>
2023-04-27 18:28:22 -0400

    add lfs, examples logic

--- gradio_app.py
+++ gradio_app.py
@@ -1,16 +1,22 @@
+import concurrent.futures
 import gradio as gr
+import os
 import torch 
-from cliptag.cliptag import CIImageKeywordGenerator
+from cliptag.cliptag import ImageKeywordGenerator
 from clip_interrogator import Config
 
-device = None
 
 FEATURES_FILE_DIR = "features/photos"
 TOP_K_LABELS = 100
 CLIP_MODELS = [
     "ViT-L-14/openai",
+    "ViT-L-14/laion2b_s32b_b82k",
     "ViT-B-32/laion2b_s34b_b79k"
 ]
+
+device = None
+examples = [os.path.join("examples", example) for example in os.listdir("examples")]
+print(examples)
 
 print("loading model")
 if torch.cuda.is_available():
@@ -27,8 +33,7 @@ outputs = []
 
 for clip_model_name in CLIP_MODELS:
     config = Config(clip_model_name=clip_model_name, caption_model_name=None, device=device)
-    keyword_generators.append(CIImageKeywordGenerator(FEATURES_FILE_DIR, config))
-    outputs.append(gr.Label(label=f"{clip_model_name} Image Keywords", num_top_classes=TOP_K_LABELS))
+    keyword_generators.append(ImageKeywordGenerator(FEATURES_FILE_DIR, config))
 
 def keywords(input_image):
     labels = []
@@ -39,11 +44,21 @@ def keywords(input_image):
     return labels
 
 print("loading interface")
-demo = gr.Interface(
-    fn=keywords, 
-    inputs=gr.Image(type='pil', label="Image"), 
-    outputs=outputs
-)
+
+with gr.Blocks() as demo:
+    gr.Markdown("Upload an image below to generate keywords using different CLIP model versions.")
+    inp = gr.Image(type='pil', label="Image")
+    gr.Examples(
+        examples=examples,
+        inputs=inp,
+    )
+    btn = gr.Button("Generate Keywords")
+    with gr.Row():
+        for clip_model_name in CLIP_MODELS:
+            output = gr.Label(label=f"{clip_model_name} Image Keywords", num_top_classes=TOP_K_LABELS)
+            outputs.append(output)
+
+    btn.click(fn=keywords, inputs=inp, outputs=outputs)
 
 if __name__ == "__main__":
     print("launching interface")
