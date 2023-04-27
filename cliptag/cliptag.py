27d2412200ecae55a813d78e11f4f922db9583d1
Matthew Tucker <matt@joinpaladin.com>
2023-04-27 17:01:19 -0400

    refactor cliptag

--- cliptag/cliptag.py
+++ cliptag/cliptag.py
@@ -3,7 +3,7 @@ from typing import List
 import torch
 import open_clip
 import os
-from clip_interrogator import Config, Interrogator, LabelTable, load_list
+from clip_interrogator import Config, Interrogator as CIInterrogator, LabelTable, load_list
 from tqdm import tqdm
 
 def load_keywords_from_files(directory):
@@ -20,59 +20,7 @@ def load_keywords_from_files(directory):
     print(f"Loaded {len(keyword_list)} keywords...")
     return keyword_list
 
-class ImageKeywordGenerator:
-    def __init__(self, model, preprocess, device, features_file_dir):
-        self.model = model
-        self.preprocess = preprocess
-        self.device = device
-        self._candidate_keyword_labels = load_keywords_from_files(features_file_dir)
-        self._candidate_keyword_features = self._encode_keywords(self._candidate_keyword_labels)
-
-    def _encode_keywords(self, candidate_keywords, batch_size=1000):
-        # Tokenize and encode the candidate keywords in batches
-        print("Tokenizing and encoding candidate keywords...")
-        text_features = []
-        for i in range(0, len(candidate_keywords), batch_size):
-            print(f"Batch {i+1}...")
-            batch_tokens = open_clip.tokenize(candidate_keywords[i:i + batch_size]).to(self.device)
-            with torch.no_grad():
-                batch_features = self.model.encode_text(batch_tokens)
-            text_features.append(batch_features)
-        # Concatenate the encoded features from all batches
-        text_features = torch.cat(text_features, dim=0)
-        return text_features
-
-    def generate_keywords_for_image(self, image, top_k=5):
-        try:
-            print("Preprocessing image...")
-            image_tensor = self.preprocess(image).unsqueeze(0).to(self.device)
-
-            # Encode the image and keywords
-            print("Encoding image and keywords...")
-            with torch.no_grad():
-                print("Encoding image...")
-                image_features = self.model.encode_image(image_tensor)
-
-            # Calculate the similarity scores
-            print("Calculating similarity scores...")
-            similarities = torch.matmul(image_features, self._candidate_keyword_features.T)
-
-            # Get the top K keywords based on the similarity scores
-            print("Getting top keywords...")
-            values, indices = torch.topk(similarities, top_k, dim=-1)
-
-            values_list = values.squeeze(0).tolist()
-            indices_list = indices.squeeze(0).tolist()
-
-            top_keywords = {self._candidate_keyword_labels[idx]: values_list[i] / 100 for i, idx in enumerate(indices_list)}
-            return top_keywords
-
-        except Exception as e:
-            print(f"Error generating keywords for image: {e}")
-            return []
-
-
-class CIInterrogator(Interrogator):
+class Interrogator(CIInterrogator):
     def __init__(self, config: Config):
         self.config = config
         self.device = config.device
@@ -104,12 +52,12 @@ class CIInterrogator(Interrogator):
             self.clip_preprocess = config.clip_preprocess
         self.tokenize = open_clip.get_tokenizer(clip_model_name)
 
-class CIImageKeywordGenerator:
+class ImageKeywordGenerator:
 
     def __init__(self, features_file_dir, config=None):
         if not config:
             config = Config(caption_model_name=None)
-        self.ci = CIInterrogator(config)
+        self.ci = Interrogator(config)
         cache_desc = features_file_dir.replace('/', '_')
         self.table = LabelTable(load_keywords_from_files(features_file_dir), cache_desc, self.ci)
         self.device = self.table.device
@@ -125,8 +73,6 @@ class CIImageKeywordGenerator:
             similarity = image_features @ text_embeds.T
             if reverse:
                 similarity = -similarity
-        # vals, top_labels = similarity.float().cpu().topk(top_count, dim=-1)
-        # return [(vals[0][i], top_labels[0][i].numpy()) for i in range(top_count)]
 
         values, indices = torch.topk(similarity, top_count, dim=-1)
 
@@ -157,4 +103,3 @@ class CIImageKeywordGenerator:
 
     def generate_keywords_for_image(self, image, top_k=5):
         return self.rank(self.ci.image_to_features(image), top_count=top_k)
-        # return {label: 1 for label in labels}
