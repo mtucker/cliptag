b92e7bf252edc1f1129771a0bb5c3096e9dac33f
Matthew Tucker <matt@joinpaladin.com>
2023-04-28 13:59:50 -0400

    reorganize cache dir, refactorings

---
+++ cliptag/loaders.py
@@ -0,0 +1,47 @@
+
+
+import os
+import pickle
+
+import torch
+from cliptag import ImageKeywordGenerator
+from clip_interrogator import Config
+
+def load_device():
+    device = None
+
+    if torch.cuda.is_available():
+        # Use the GPU (CUDA) as the device
+        device = torch.device("cuda")
+        print("Using GPU:", torch.cuda.get_device_name(0))
+    else:
+        # Use the CPU as the device
+        device = torch.device("cpu")
+        print("Using CPU")
+
+    return device
+
+def load_clip_generators(clip_model_names, feature_files_dir, device, cache_dir):
+  """
+  Load models from file or create new ones
+  :return: list of ImageKeywordGenerator objects
+  """
+  generator_cache_dir = f"{cache_dir}/generators"
+  embeds_cache_dir = f"{cache_dir}/embeds"
+  pickled_generators = f"{generator_cache_dir}/keyword_generators.pkl" 
+
+  if os.path.exists(pickled_generators):
+      print("Loading models from file")
+      with open(pickled_generators, "rb") as f:
+          keyword_generators = pickle.load(f)
+  else:
+      keyword_generators = []
+
+      for clip_model_name in clip_model_names:
+          config = Config(clip_model_name=clip_model_name, caption_model_name=None, device=device, cache_dir=embeds_cache_dir)
+          keyword_generators.append(ImageKeywordGenerator(feature_files_dir, config))
+
+      with open(pickled_generators, "wb") as f:
+          pickle.dump(keyword_generators, f)
+
+  return keyword_generators
--- gradio_app.py
+++ gradio_app.py
@@ -3,10 +3,12 @@ import gradio as gr
 import os
 import torch 
 from cliptag.cliptag import ImageKeywordGenerator
+from cliptag.loaders import load_clip_generators, load_device
 from clip_interrogator import Config
 
 
 FEATURES_FILE_DIR = "features/photos"
+CACHE_DIR = "cache"
 TOP_K_LABELS = 100
 CLIP_MODELS = [
     "ViT-L-14/openai",
@@ -14,39 +16,13 @@ CLIP_MODELS = [
     "ViT-B-32/laion2b_s34b_b79k"
 ]
 
-device = None
-examples = [os.path.join("examples", example) for example in os.listdir("examples")]
-print(examples)
-
-print("loading model")
-if torch.cuda.is_available():
-    # Use the GPU (CUDA) as the device
-    device = torch.device("cuda")
-    print("Using GPU:", torch.cuda.get_device_name(0))
-else:
-    # Use the CPU as the device
-    device = torch.device("cpu")
-    print("Using CPU")
-
-keyword_generators = []
-outputs = []
-
-for clip_model_name in CLIP_MODELS:
-    config = Config(clip_model_name=clip_model_name, caption_model_name=None, device=device)
-    keyword_generators.append(ImageKeywordGenerator(FEATURES_FILE_DIR, config))
-
-def keywords(input_image):
-    labels = []
-
-    for keyword_generator in keyword_generators:
-        labels.append(keyword_generator.generate_keywords_for_image(input_image, top_k=TOP_K_LABELS))
-
-    return labels
+device = load_device()
+keyword_generators = load_clip_generators(CLIP_MODELS, FEATURES_FILE_DIR, device, CACHE_DIR)
 
 def generate_keywords(keyword_generator, input_image, top_k):
     return keyword_generator.generate_keywords_for_image(input_image, top_k=top_k)
 
-def keywords_para(input_image):
+def keywords(input_image):
     labels = []
 
     # Use ThreadPoolExecutor to parallelize keyword generation
@@ -60,7 +36,9 @@ def keywords_para(input_image):
 
     return labels
 
-print("loading interface")
+print("Loading interfacez...")
+outputs = []
+examples = [os.path.join("examples", example) for example in os.listdir("examples")]
 
 with gr.Blocks() as demo:
     gr.Markdown("Upload an image below to generate keywords using different CLIP model versions.")
@@ -75,8 +53,8 @@ with gr.Blocks() as demo:
             output = gr.Label(label=f"{clip_model_name} Image Keywords", num_top_classes=TOP_K_LABELS)
             outputs.append(output)
 
-    btn.click(fn=keywords_para, inputs=inp, outputs=outputs)
+    btn.click(fn=keywords, inputs=inp, outputs=outputs)
 
 if __name__ == "__main__":
-    print("launching interface")
+    print("Launching interface...")
     demo.launch()
